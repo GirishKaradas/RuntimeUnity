0d9c2ca7f65011a22c4299234b2a16068cbeebba
Girish Karadas <girishkaradas@gmail.com>
2021-03-01 14:51:03 +0530

    Initial commit

---
+++ src/main/java/com/genius/runtimeunity/MainActivity.java
@@ -0,0 +1,26 @@
+package com.genius.runtimeunity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.glass.ui.GlassGestureDetector;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onGesture(GlassGestureDetector.Gesture gesture) {
+        switch (gesture){
+            case TAP:
+                startActivity(new Intent(MainActivity.this, UnityPlayerActivity.class));
+                finish();
+                break;
+        }
+        return super.onGesture(gesture);
+    }
+}
